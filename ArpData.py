b818c671cd95dfb5026c40f0f4cb2b50ede6683d
RivaAckerman <31956791+RivaAckerman@users.noreply.github.com>
2017-09-26 19:54:22 +0800

    Add files via upload

---
+++ ArpData.py
@@ -0,0 +1,32 @@
+import os
+import time
+import pyodbc
+import datetime
+import sys
+
+
+while True:
+        conn=pyodbc.connect('DRIVER={FreeTDS};SERVER=172.16.7.1;port=1433;DATABASE=shuihu;UID=sa;PWD=666666;TDS_Version=8.0;')
+        print 'connect ok......'
+        os.system('arp -a > ArpD.txt')
+        f=open("ArpD.txt","r")
+        cursor=conn.cursor()
+        print "Saving new ARP cache data-----"
+		i=datetime.datetime.now()
+        for line in f:
+                line_d=line.split(" ");
+                a=line_d[1]
+                b=line_d[3]
+                c=line_d[5]
+                sql = "INSERT INTO ArpSave(ip,mac,dtype,ctime) VALUES('%s','%s','%s','%s')"%(a,b,c,i)
+                cursor.execute(sql)
+                conn.commit()
+		print "Saving OK"
+		os.system('ip neigh flush  dev eth0')
+        os.system('ip neigh flush  dev wlan0')
+        print "ARP cache table data has been deleted"
+        f.close()
+        cursor.close()
+        conn.close()
+        print "deleted OK"
+        time.sleep(60)
